b667d8b567b268e80ab30b23637b1bae30701a3a
Juhwan Yun <btakeya.dev@gmail.com>
2016-08-06 20:09:20 +0900

    Add test interface for about Yahoo Chart API
    - TODO: Yahoo Chart API Connector, Yahoo Chart API Model, and so on

---
+++ test/test_YahooChartApi.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest import main as test_main
+from Utility import *
+
+
+class TestYahooChartApi(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_not_defined_yet(self):
+        pass
+
+    def tearDown(self):
+        pass
